fcf3c58eab33f470e48d4d7a54eceace37a5408c
Jamie Mansfield <malgm@lapis.blue>
2015-03-26 19:01:10 +0000

    Opps forgot to stage setup.py :|

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='histacim_docs_theme',
+    version='0.0.1',
+    packages=['histacom_docs_theme'],
+    url='',
+    license='',
+    author='jamierocks',
+    author_email='malgm@lapis.blue',
+    description='The Histacom documentation theme'
+)
